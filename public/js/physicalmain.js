10c3762429b286ea19aa871015168da2f3aa0ee5
Stephan Procopii <stephanp@live.unc.edu>
2022-05-01 14:41:45 -0400

    undefined popped up when pressing tasks for today physical button

--- public/js/physicalmain.js
+++ public/js/physicalmain.js
@@ -5,8 +5,9 @@
 const physicaltask=document.getElementById("physicaltask")
 
 // event listener for click
-physicaltask.addEventListener("click", getPhysTasks)
-// function to wait for response
+window.addEventListener('load',function(){
+    physicaltask.addEventListener("click", getPhysTasks)
+});// function to wait for response
 
 async function getPhysTasks(event) {
     event.preventDefault()
@@ -14,15 +15,15 @@ async function getPhysTasks(event) {
     const endpoint='/app/physical'
     const url=document.baseURI+endpoint
     // waiting for response
-    await fetch(url)
+    await fetch(url, {mode: 'cors'})
     // receiving response
                     .then(function(response) {
-                        return response.json();
+                        return response.text();
                     })
                     
-                    .then(function(result) {
-                        console.log(result);
-                        document.getElementById("physicalres").innerHTML = result.tasks;
+                    .then(function(response) {
+                        console.log(response);
+                        document.getElementById("physicalres").innerHTML = response.tasks;
             
                     });
 } 
@@ -30,15 +31,17 @@ async function getPhysTasks(event) {
 const ptask1=document.getElementById("ptask1")
 
 // event listener for click
-physicaltask.addEventListener("click", getTask1Des)
-// function to wait for response
+window.addEventListener('load',function(){
+    ptask1.addEventListener("click", getTask1Des)
+});// function to wait for response
 
 async function getTask1Des() {
     // endpoint
-    const endpoint="app/physical/task1/"
-    const url=document.baseURI+endpoint
+    const endpoint='/app/physical/task1'
+    const base = 'http://localhost:3000/physicaltasks.html'
+    const url=document.base+endpoint
     // waiting for response
-    await fetch(url)
+    await fetch(url, {mode: 'cors'})
     // receiving response
                     .then(function(response) {
                         return response.json();
@@ -55,15 +58,17 @@ async function getTask1Des() {
 const ptask2=document.getElementById("ptask2")
 
 // event listener for click
-physicaltask.addEventListener("click", getTask2Des)
-// function to wait for response
+window.addEventListener('load',function(){
+    ptask2.addEventListener("click", getTask2Des)
+});// function to wait for response
 
 async function getTask2Des() {
     // endpoint
-    const endpoint="app/physical/task2/"
-    const url=document.baseURI+endpoint
+    const endpoint='/app/physical/task2'
+    const base = 'http://localhost:3000/physicaltasks.html'
+    const url=document.base+endpoint
     // waiting for response
-    await fetch(url)
+    await fetch(url, {mode: 'cors'})
     // receiving response
                     .then(function(response) {
                         return response.json();
@@ -81,15 +86,17 @@ async function getTask2Des() {
 const ptask3=document.getElementById("ptask3")
 
 // event listener for click
-physicaltask.addEventListener("click", getTask3Des)
-// function to wait for response
+window.addEventListener('load',function(){
+    ptask3.addEventListener("click", getTask3Des)
+});// function to wait for response
 
 async function getTask3Des() {
     // endpoint
-    const endpoint="app/physical/task3/"
-    const url=document.baseURI+endpoint
+    const endpoint='/app/physical/task3'
+    const base = 'http://localhost:3000/physicaltasks.html'
+    const url=document.base+endpoint
     // waiting for response
-    await fetch(url)
+    await fetch(url, {mode: 'cors'})
     // receiving response
                     .then(function(response) {
                         return response.json();
@@ -101,3 +108,4 @@ async function getTask3Des() {
             
                     });
 } 
+
